6b7c2236d39875502c729554ecb48b8589e9196e
ppaulis <pascal.paulis@continuousphp.com>
2015-06-08 12:15:41 +0200

    adding apigility

--- config/application.config.php
+++ config/application.config.php
@@ -7,6 +7,19 @@ return array(
         'ZfcAdmin',
         'BsbPhingService',
         'CphpAgent',
+        'ZF\Apigility',
+        'ZF\Apigility\Provider',
+        'AssetManager',
+        'ZF\ApiProblem',
+        'ZF\MvcAuth',
+        'ZF\OAuth2',
+        'ZF\Hal',
+        'ZF\ContentNegotiation',
+        'ZF\ContentValidation',
+        'ZF\Rest',
+        'ZF\Rpc',
+        'ZF\Versioning',
+        'ZF\DevelopmentMode'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
